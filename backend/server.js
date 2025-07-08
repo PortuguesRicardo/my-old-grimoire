cf02303c159538ba4aa71fb08c8f20aba4b9ec55
ricardo_batista_portugues@hotmail.com <ricardo_batista_portugues@hotmail.com>
2025-07-08 12:53:59 +0100

    linking route to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+const authRoutes = require('./routes/auth'); //wiring auth route
+
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -10,6 +12,8 @@ const app = express();
 app.use(express.json());
 
 // Test Route
+
+app.use('/api/auth', authRoutes);
 app.get('/', (req, res) => {
     res.send('Server is running!');
 });
@@ -23,7 +27,7 @@ mongoose.connect(process.env.MONGO_URI, {
     .catch(err => console.log(err));
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5500;  //environment port variable
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
