b94f3acc69c9c1423cc170d7a11e66c6fbb99d56
ricardo_batista_portugues@hotmail.com <ricardo_batista_portugues@hotmail.com>
2025-07-09 21:15:07 +0100

    minor syntax fix

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,18 +1,18 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    try {
-        // Extract the token from the "Authorization" header
-        const authHeader = req.headers.authorization;
-        if (!authHeader) {
-            return res.status(401).json({ error: 'Missing authorization header' });
-        }
-
-        const token = authHeader.split(' ')[1]; // Expecting format: Bearer <token>
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        req.auth = { userId: decodedToken.userId }; // Attach user info to request
-        next(); // Proceed
-    } catch (error) {
-        res.status(401).json({ error: 'Invalid or expired token' });
+  try {
+    // Extract the token from the "Authorization" header
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).json({ error: 'Missing authorization header' });
     }
+
+    const token = authHeader.split(' ')[1]; // Expecting format: Bearer <token>
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.auth = { userId: decodedToken.userId }; // Attach user info to request
+    next(); // Proceed
+  } catch (error) {
+    res.status(401).json({ error: 'Invalid or expired token' });
+  }
 };
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -2,14 +2,14 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, 'images'); // files go to /backend/images
-    },
-    filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
-        const extension = path.extname(file.originalname);
-        callback(null, name + Date.now() + extension);
-    },
+  destination: (req, file, callback) => {
+    callback(null, 'images'); // files go to /backend/images
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(' ').join('_');
+    const extension = path.extname(file.originalname);
+    callback(null, name + Date.now() + extension);
+  },
 });
 
 module.exports = multer({ storage }).single('image');
