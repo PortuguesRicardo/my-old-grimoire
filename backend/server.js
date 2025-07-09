956d98b5838a49ff21a7bc76e44a97152c1137f5
ricardo_batista_portugues@hotmail.com <ricardo_batista_portugues@hotmail.com>
2025-07-09 21:12:25 +0100

    change on port variable and added cors

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
-const cors = require('cors');  // to allow request from backend to frontend in different ports. 
-const authRoutes = require('./routes/auth'); //wiring auth route
+const cors = require('cors'); // to allow request from backend to frontend in different ports.
 
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const testRoutes = require('./routes/test'); // testing token on Postman
 const path = require('path'); // so frontend can load uploaded images
-const bookRoutes = require('./routes/bookRoutes'); // to get books
+const testRoutes = require('./routes/test'); // testing token on Postman
+const authRoutes = require('./routes/auth'); // wiring auth route
+const bookRoutes = require('./routes/bookRoutes');
+// to get books
 dotenv.config();
 
 const app = express();
@@ -21,19 +22,19 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/test', testRoutes);
 app.use('/api/books', bookRoutes);
 app.get('/', (req, res) => {
-    res.send('Server is running!');
+  res.send('Server is running!');
 });
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
 
 // Start server
-const PORT = process.env.PORT || 5500;  //environment port variable
+const PORT = process.env.PORT || 5500; // environment port variable
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
