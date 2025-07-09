94d78320dd0a38c59672935857903056614ca2f3
ricardo_batista_portugues@hotmail.com <ricardo_batista_portugues@hotmail.com>
2025-07-09 19:20:27 +0100

    adding Cors to allow requests from backend to frontend in different ports.

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const cors = require('cors');  // to allow request from backend to frontend in different ports. 
 const authRoutes = require('./routes/auth'); //wiring auth route
 
 const express = require('express');
@@ -14,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 // Test Route
-
+app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/test', testRoutes);
