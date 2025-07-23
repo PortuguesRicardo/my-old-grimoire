86d22802dac3f430b27f1ddf53ae028c9f6cf5f3
ricardo_batista_portugues@hotmail.com <ricardo_batista_portugues@hotmail.com>
2025-07-23 14:57:47 +0100

    wrapping routes inside a layoutWrapper and using useLocation() to check current path so that header and footer do not render inside path /login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {
-  BrowserRouter, Route, Routes,
+  BrowserRouter, Route, Routes, useLocation,
 } from 'react-router-dom';
 import SignIn from './pages/SignIn/SignIn';
 import Home from './pages/Home/Home';
@@ -13,27 +13,38 @@ import UpdateBook from './pages/updateBook/UpdateBook';
 import { useUser } from './lib/customHooks';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
-function App() {
+function LayoutWrapper() {
+  const location = useLocation();
   const [user, setUser] = useState(null);
   const { connectedUser } = useUser();
 
   useEffect(() => {
     setUser(connectedUser);
   }, [connectedUser]);
+
+  // Hide Header/Footer on login page
+  const hideLayout = location.pathname === APP_ROUTES.SIGN_IN;
+
+  return (
+    <>
+      <ScrollToTop />
+      {!hideLayout && <Header user={user} setUser={setUser} />}
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path={APP_ROUTES.SIGN_IN} element={<SignIn setUser={setUser} />} />
+        <Route path={APP_ROUTES.BOOK} element={<Book />} />
+        <Route path={APP_ROUTES.UPDATE_BOOK} element={<UpdateBook />} />
+        <Route path={APP_ROUTES.ADD_BOOK} element={<AddBook />} />
+      </Routes>
+      {!hideLayout && <Footer />}
+    </>
+  );
+}
+
+function App() {
   return (
     <BrowserRouter>
-      <div>
-        <ScrollToTop />
-        <Header user={user} setUser={setUser} />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path={APP_ROUTES.SIGN_IN} element={<SignIn setUser={setUser} />} />
-          <Route path={APP_ROUTES.BOOK} element={<Book />} />
-          <Route path={APP_ROUTES.UPDATE_BOOK} element={<UpdateBook />} />
-          <Route path={APP_ROUTES.ADD_BOOK} element={<AddBook />} />
-        </Routes>
-        <Footer />
-      </div>
+      <LayoutWrapper />
     </BrowserRouter>
   );
 }
